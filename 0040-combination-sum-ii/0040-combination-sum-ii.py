2458a8b76f94707f07c6a0afb7ae13144b9592dc
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-03-16 17:08:46 +0300

    Time: 93 ms (37.41%), Space: 14.1 MB (16.79%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        visited = set()
+        result = []
+        
+        def backtrack(lst, candsum, idx):
+            
+            if candsum == target:
+                result.append(lst[:])
+                return
+            
+            if idx >= len(candidates):
+                return
+            
+            if candsum > target:
+                return
+            
+            for i in range(idx, len(candidates)):
+                                 
+                if i > idx and candidates[i] == candidates[i-1]:
+                        continue
+                lst.append(candidates[i])
+                candsum += candidates[i]
+
+                backtrack(lst, candsum, i + 1)
+                
+                back = lst.pop()
+                candsum -= back
+                    # visited.remove(candidates[i])
+        
+        candidates.sort()
+        
+        backtrack([], 0, 0)
+        
+        return result
+                
