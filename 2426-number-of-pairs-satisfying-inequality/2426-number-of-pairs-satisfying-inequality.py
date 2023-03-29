c7126e3aeb2f9f3252599d790e90539bf46b761f
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-03-29 05:42:25 +0300

    Time: 2423 ms (15.72%), Space: 32.7 MB (75.11%) - LeetHub

--- 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
@@ -1,89 +1,53 @@
 class Solution:
-    def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:        
+        count = 0
         merged = []
-        new = []
+        
         for i in range(len(nums1)):
             merged.append(nums1[i] - nums2[i])
-        
-        def mergesort(left, right):
-            if left == right:
-                return [0, [merged[left]]]
-            
-            mid = left + (right - left)//2
-            leftcount, leftarr = mergesort(left, mid)
-            rightcount, rightarr = mergesort(mid+1, right)
-            ans = leftcount + rightcount
-            n = len(rightarr)
-            for num in leftarr:
-                ans += (n-bisect_left(rightarr, num - diff))
-            
-            return [ans, sorted(leftarr+rightarr)]
-        
-        return mergesort(0, len(merged)-1)[0]
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         count = 0
-#         merged = []
-#         new = []
-#         for i in range(len(nums1)):
-#             merged.append(nums1[i] - nums2[i])
                     
-#         def mergesort(arr):
-#             nonlocal count
+        def mergesort(arr):
+            nonlocal count
             
-#             if len(arr) <= 1:
-#                 return arr
+            if len(arr) <= 1:
+                return arr
               
-#             mid = len(arr)//2
-#             left = mergesort(arr[:mid])
-#             right = mergesort(arr[mid:])
+            mid = len(arr)//2
+            left = mergesort(arr[:mid])
+            right = mergesort(arr[mid:])
             
             
                 
-#             i, j = (len(left) - 1), 0
-#             while j < len(right):
-#                 while i >= 0:
-#                         if left[i] <= right[j] + diff:
-#                             count += (i + 1)
-#                             break
-#                         i -= 1  
-#                 j += 1
+            j = 0
+            while j < len(right):
+                ans = bisect_right(left, (right[j]+diff))
+                count += ans
+                j += 1
                         
-                  
-#             # l,r = 0,0
-# #             while l < len(left) and r < len(right):
-# #                     if left[l] < right[r]:
-# #                         new.append(left[l])
-# #                         l += 1
-# #                     else:
-# #                         new.append(right[r])
-# #                         r += 1
+            new = []      
+            l,r = 0,0
+            while l < len(left) and r < len(right):
+                    if left[l] < right[r]:
+                        new.append(left[l])
+                        l += 1
+                    else:
+                        new.append(right[r])
+                        r += 1
                         
-# #             while l < len(left):
-# #                         new.append(left[l])
-# #                         l += 1
+            while l < len(left):
+                        new.append(left[l])
+                        l += 1
                 
-# #             while r < len(right):
-# #                     new.append(right[r])
-# #                     r += 1
-                
-#             return sorted(left+right)
+            while r < len(right):
+                    new.append(right[r])
+                    r += 1
             
-#         print(mergesort(merged))
             
-#         return count
+            return new
+            
+        print(mergesort(merged))
+            
+        return count
 
                 
                         
