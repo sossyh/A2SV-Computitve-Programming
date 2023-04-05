ea0999aaaa0cd38b970b299c934f61d5653ed6b4
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-04-05 15:48:00 +0300

    Time: 234 ms (62.57%), Space: 27 MB (5.19%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -4,11 +4,12 @@ class Solution:
         visited = set()
         
         def backtrack(lst, idx):
-            if len(lst) >= 2 and tuple(lst) not in visited and lst == sorted(lst):
+            if len(lst) >= 2 and tuple(lst) not in visited:
                 visited.add(tuple(lst))
                 result.append(lst[:])
             
             for i in range(idx,len(nums)):
+                if not lst or lst[-1] <= nums[i]:
                     lst.append(nums[i])
                     backtrack(lst, i + 1)
                     lst.pop()
