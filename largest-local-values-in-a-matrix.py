8162ab430f19b1c2f459c66053ad25b6bcb6413b
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-04-14 14:37:15 +0300

    added largest-local-values-in-a-matrix.python

---
+++ largest-local-values-in-a-matrix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxelement(self, grid):
+        flatten = []
+        for i in range(len(grid)):
+            flatten += grid[i]
+        return max(flatten)
+
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        result = [[0] * (len(grid)-2) for i in range(len(grid)-2)]
+
+        for i in range(len(result)):
+            for j in range(len(result)):
+                newgrid = [ l[j: j+3] for l in grid[i : i + 3]]
+                print(newgrid)
+                maxe = self.maxelement(newgrid)
+                result[i][j] = maxe
+
+        return result
