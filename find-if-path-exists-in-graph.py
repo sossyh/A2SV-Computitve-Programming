8f968c9bc96f8e4a2abe3d550f8788671ba5d333
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-05-17 18:44:35 +0300

    added find-if-path-exists-in-graph.python

--- find-if-path-exists-in-graph.py
+++ find-if-path-exists-in-graph.py
@@ -1,24 +1,42 @@
+class Disjoint:
+
+    def __init__(self, size):
+        self.rep = [ i for i in range(size)]
+        self.rank = [ 1 for i in range(size)]
+    
+    def find(self, x):
+
+        if self.rep[x] == x:
+            return x
+        
+        the_rep = self.find(self.rep[x])
+        self.rep[x] = the_rep
+
+        return the_rep
+
+    def union(self, x, y):
+        repx = self.find(x)
+        repy = self.find(y)
+
+        rankx = self.rank[x]
+        ranky = self.rank[y]
+
+        if rankx < ranky:
+            self.rep[repx] = repy
+            self.rank[repy] = max(self.rank[repy], 1 + self.rank[repx])
+        
+        else:
+            self.rep[repy] = repx
+            self.rank[repx] = max(self.rank[repx], 1 + self.rank[repy])
+    
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = collections.defaultdict(list)
-        ans = collections.defaultdict(int)
+        unionFind = Disjoint(n)
 
-        for i in edges:
-            graph[i[0]].append(i[1])
-            graph[i[1]].append(i[0])  
-        visited = set()
+        for i, j in edges:
+            unionFind.union(i, j)
         
-        def dfs(vertex, parent):
-            
-            ans[vertex] = parent
-            visited.add(vertex)
-            
-            for i in graph[vertex]:
-                if i not in visited:
-                    dfs(i, parent)
-                        
-        for i in range(n):
-            if i not in visited:
-                dfs(i, i)
-            
-        return ans[source] == ans[destination]
+        return unionFind.connected(source, destination)
