4c477230ea16ca4c6a03a9241515802b1d9306fc
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-07-21 05:42:46 +0300

    Time: 46 ms (59.07%), Space: 16.3 MB (32.21%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,12 +1,17 @@
 class Solution:
-    @cache
     def numTrees(self, n: int) -> int:
-        if n <= 1:
-            return 1
         
-        ans = 0
+        cache = [1] * (n+1)
         
-        for i in range(1, n+1):
-            ans += self.numTrees(i-1) * self.numTrees(n-i)
+        for node in range(2, n+1):
+            summ = 0
+            
+            for root in range(1, node+1):
+                left = root - 1
+                right = node - root
+                summ += cache[left] * cache[right]
+            
+            cache[node] = summ
         
-        return ans
+        return cache[-1]
+            
