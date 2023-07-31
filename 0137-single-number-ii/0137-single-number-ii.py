0335387f43e0de10cf0314d2e1509ca461d529ea
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-07-31 16:57:12 +0300

    Time: 122 ms (26.85%), Space: 18.1 MB (79.36%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,8 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        d = Counter(nums)
+        ans = 0
+
+        for i in range(32):
+            c = 0
+            for num in nums:
+                if num < 0:
+                    num = num & (2**32-1)
+                c += (num >> i) & 1
+                
+            c %= 3
+            ans |= c << i
+
         
-        for i in d:
-            if d[i] == 1:
-                return i
-        
+        if ans >= 2**31:
+            ans -= 2**32
+
+        return ans
