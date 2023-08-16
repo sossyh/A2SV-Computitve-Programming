c1b75ce6f1a7a4cbdae3693731baab194f8181b6
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-08-16 15:58:20 +0300

    Time: 1006 ms (5.02%), Space: 65 MB (6.83%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,20 +1,20 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         result = []
+        # nums = [i+1 for i in range(n)]
         
-        def backTrack(lst, num):
-            if len(lst) == k:
-                result.append(lst.copy())
+        def backTrack(lst, idx):   
+            if idx >= n:
+                if len(lst) == k:
+                    result.append(lst[:])
                 return
-            for i in range(num, n+1):
-                # appending candidates
-                lst.append(i)
-                # backtarcking
-                backTrack(lst, i+1)
-                
-                lst.pop()
+           
+            lst.append(idx+1)
+            backTrack(lst, idx+1)   
+            lst.pop()
+            backTrack(lst, idx+1)
         
-        backTrack([], 1)
+        backTrack([], 0)
         
         return result
                 
