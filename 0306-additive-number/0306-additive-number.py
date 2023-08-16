1c773133acf63b671bf77a2e71e681e3a202100c
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-08-16 04:41:08 +0300

    Time: 29 ms (99.42%), Space: 16.3 MB (83.43%) - LeetHub

---
+++ 0306-additive-number/0306-additive-number.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+        sequence = []
+        if len(num) <= 2:
+            return False
+        
+        def backtrack(idx, num):
+            if num == "":
+                return True
+            
+            
+            for i in range(len(num)):
+                prefix = num[:i+1]
+                if idx <= 1:
+                    if prefix == '0' or prefix[0] != '0':
+                        sequence.append(int(prefix))
+                        if backtrack(idx+1, num[i+1:]) and len(sequence) >= 3:
+                            return True
+                        print(sequence)
+                        sequence.pop()
+                else:
+                    if prefix == '0' or prefix[0] != '0':
+                        if int(prefix) == sequence[idx-1] + sequence[idx-2]:
+                            sequence.append(int(prefix))
+                            if backtrack(idx+1, num[i+1:]):
+                                return True
+                            print(sequence)
+                            sequence.pop()
+        
+        
+        
+        return backtrack(0, num)    
+            
