2b012318a0d7567cf561007c9506567e8b4065a6
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-08-20 02:31:08 +0300

    Time: 901 ms (22.03%), Space: 16.5 MB (30.51%) - LeetHub

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.py
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.py
@@ -8,12 +8,15 @@ class Solution:
         count = 0
         
         for i in range(len(nums)):
-            gcdf = (nums[i])
-            m = 0
-            for j in range(i,len(nums)):
-                gcdf = self.gcd(gcdf,nums[j])
-                if gcdf == k:
+            currgcd = nums[i]
+            for j in range(i, len(nums)):
+                a =self.gcd(currgcd, nums[j]) 
+                if a == k:
                     count += 1
+                currgcd = a
+                    
+                
+                
             
         return count
                 
