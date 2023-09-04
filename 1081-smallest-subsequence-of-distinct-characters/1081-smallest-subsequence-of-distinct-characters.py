16ba3b6963f7e62162fb920ae72f6d5063fe72d1
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-09-04 09:48:00 +0300

    Time: 40 ms (70.19%), Space: 16.3 MB (29.16%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.py
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.py
@@ -7,14 +7,15 @@ class Solution:
         for c in s:
             freq[c] -= 1
             
-            if not visited & (1<<(ord(c))):
+            if visited & (1<<(ord(c))):
+                continue
             
-                while stack and freq[stack[-1]] > 0 and stack[-1] > c:
-                    a = stack.pop()
-                    visited ^= (1 << (ord(a)))
+            while stack and freq[stack[-1]] > 0 and stack[-1] > c:
+                a = stack.pop()
+                visited ^= (1 << (ord(a)))
 
-                stack.append(c)
-                visited |= (1 << ord(c))
+            stack.append(c)
+            visited |= (1 << ord(c))
         
         
         return "".join(stack)
