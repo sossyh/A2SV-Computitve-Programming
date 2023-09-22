9142a9a1bfa53eaef387aaabf1aff526a59bbd1a
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-09-22 10:54:00 +0300

    Time: 39 ms (57.81%), Space: 16.2 MB (66.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,10 +1,5 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        
-        if len(nums) == 2:
-            return max(nums[0], nums[1])
         
         @cache
         def dp(idx):
