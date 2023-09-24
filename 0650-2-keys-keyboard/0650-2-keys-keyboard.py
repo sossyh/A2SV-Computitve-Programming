4b64f24c1bf89d5518fd1a95b426dbe847756fca
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-09-24 10:07:41 +0300

    Time: 218 ms (34.86%), Space: 16.2 MB (92.27%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -13,22 +13,22 @@ class Solution:
     
     def minSteps(self, n: int) -> int:
         
-        keyboard = [i for i in range(n+1)]
+        dp = [i for i in range(n+1)]
         
         for i in range(n+1):
             if i == 1:
-                keyboard[i] = 0
+                dp[i] = 0
             elif i == 2:
-                keyboard[i] = 2
+                dp[i] = 2
             
             elif self.devisors(i) == "prime":
-                keyboard[i] = i
+                dp[i] = i
             
             else:
                 a = self.devisors(i)
                 b = i // a
-                keyboard[i] = keyboard[a] + b
+                dp[i] = dp[a] + b
         
         
-        return keyboard[n]
+        return dp[n]
         
