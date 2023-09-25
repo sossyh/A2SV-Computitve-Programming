a85869819f1ae4d93f9080d1205da9916b10f072
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-09-25 14:06:52 +0300

    Time: 2485 ms (11.87%), Space: 16.7 MB (39.55%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,29 +1,27 @@
 class Solution:
-    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-        agescore = [(ages[i], scores[i]) for i in range(len(scores))]
-        agescore.sort()
-        # print(agescore)
-        memo = {}
+    def dp(self,idx, agescore, prevage, prevscore, memo):
         
-        def dp(idx, prevage, prevscore):
             if idx in memo:
                 return memo[idx]
             
-            if idx >= len(scores):
+            if idx >= len(agescore):
                 return 0
             
             maxscore = 0
             
-            for i in range(idx, len(scores)):
-                if not prevscore or (agescore[i][0] > prevage and agescore[i][1] >= prevscore) or (agescore[i][0] == prevage): 
-                    
-                    # if agescore[i][0] == prevage:
-                        
-                    a = dp(i+1, agescore[i][0], agescore[i][1])
+            for i in range(idx, len(agescore)):
+                if not prevscore or (agescore[i][0] > prevage and agescore[i][1] >= prevscore) or (agescore[i][0] == prevage):      
+                    a = self.dp(i+1, agescore, agescore[i][0], agescore[i][1], memo)
                     maxscore = max(maxscore, a + agescore[i][1])
             
             memo[idx] = maxscore
             return maxscore
         
-        return dp(0, None, None)
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+        agescore = [(ages[i], scores[i]) for i in range(len(scores))]
+        agescore.sort()
+
+        memo = {}            
+        
+        return self.dp(0, agescore, None, None, memo)
                 
