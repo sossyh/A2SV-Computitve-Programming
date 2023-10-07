69b60e117687914ede795c0b055e3c49a1285363
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-10-07 13:20:28 +0300

    Time: 39 ms (95.90%), Space: 17.8 MB (92.17%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.py
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -8,27 +8,26 @@ class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         if not root:
             return []
-            
+        
         result = []
+        def find_path(root, total, summ ):
+                total += root.val
+                summ.append(root.val)
+                
+                if not root.left and not root.right:
+                    if total == targetSum:
+                        result.append(summ[:])
+                    return
+                
+                if root.left:
+                    find_path(root.left, total, summ)
+                    summ.pop()
+                
+                if root.right:
+                    find_path(root.right, total, summ)
+                    summ.pop()
         
-        def path(root, candidates):
-            candidates.append(root.val)
-            if not root.left and not root.right:
-                if sum(candidates) == targetSum:
-                    result.append(candidates[:])
-                print(candidates)
-                # candidates.pop()
-                return
-            
-            if root.left:
-                path(root.left, candidates)
-                candidates.pop()
-            
-            if root.right:
-                path(root.right, candidates)
-                candidates.pop()
-            
+        find_path(root, 0, [])
         
-        path(root, [])
-
         return result
+        
