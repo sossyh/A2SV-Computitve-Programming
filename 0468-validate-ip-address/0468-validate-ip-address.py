27c71c29e8c8386c034fc81e2c2ed5f8b7ba45f5
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-10-09 03:48:34 +0300

    Time: 36 ms (73.14%), Space: 16.2 MB (94.32%) - LeetHub

--- 0468-validate-ip-address/0468-validate-ip-address.py
+++ 0468-validate-ip-address/0468-validate-ip-address.py
@@ -3,10 +3,10 @@ class Solution:
         ipv4 = r'^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])){3}$'
         ipv6 = r'^([0-9A-Fa-f]){1,4}:([0-9A-Fa-f]){1,4}:([0-9A-Fa-f]){1,4}:([0-9A-Fa-f]){1,4}:([0-9A-Fa-f]){1,4}:([0-9A-Fa-f]){1,4}:([0-9A-Fa-f]){1,4}:([0-9A-Fa-f]){1,4}$'
         
-        if re.match(ipv6, queryIP):
+        if re.search(ipv6, queryIP):
             return "IPv6"
         
-        if re.match(ipv4, queryIP):
+        if re.search(ipv4, queryIP):
             return "IPv4"
         
         return "Neither"
