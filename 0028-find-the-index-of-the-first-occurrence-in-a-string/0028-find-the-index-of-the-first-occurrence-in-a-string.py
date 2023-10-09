5383e89f8d6a90dbe4eabb8e3af5e9839c97a570
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-10-09 13:48:27 +0300

    Time: 48 ms (5.98%), Space: 16.2 MB (82.07%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -15,22 +15,16 @@ class Solution:
         curr_hash = 0
         start = 0
         
-        for end in range(len(needle)):
+        for end in range(len(haystack)):
             i = haystack[end]
             curr_hash = (curr_hash * 27) + (ord(i) - 96)
             
-        if needles_hash == curr_hash:
-                return start
+            if end >= a:
+                j = ord(haystack[start]) - 96
+                # print(end, haystack[start:end+1])
+                curr_hash -= (j * (27 ** (a))) 
+                start += 1
             
-        for end in range(len(needle), len(haystack)):
-            i = haystack[end]
-            curr_hash = (curr_hash * 27) + (ord(i) - 96)
-            
-            
-            j = ord(haystack[start]) - 96
-            curr_hash -= (j * (27 ** (a))) 
-            start += 1
-                
             if needles_hash == curr_hash:
                 return start
         
