0663a7019c9e1132fb67f90f49bc9f171091572d
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-10-09 12:05:41 +0300

    Time: 38 ms (57.24%), Space: 16.3 MB (18.98%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,17 +1,37 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) > len(haystack):
+            return -1
         
-        index = inf
+        a = len(needle)
+        k = a
+        needles_hash = 0
         
+        for i in needle:
+            needles_hash += (ord(i) - 96) * (27 ** (k - 1))
+            # needles_hash %= (10**9 + 7)
+            k -= 1
+        
+        curr_hash = 0
         start = 0
         
-        for end in range(len(haystack)):
+        for end in range(len(needle)):
+            i = haystack[end]
+            curr_hash = (curr_hash * 27) + (ord(i) - 96)
             
-            if end >= len(needle):
-                start += 1
+        if needles_hash == curr_hash:
+                return start
+            
+        for end in range(len(needle), len(haystack)):
+            i = haystack[end]
+            curr_hash = (curr_hash * 27) + (ord(i) - 96)
+            
+            
+            j = ord(haystack[start]) - 96
+            curr_hash -= (j * (27 ** (a))) 
+            start += 1
                 
-            if haystack[start: end + 1] == needle and start < index:
-                index = start
+            if needles_hash == curr_hash:
+                return start
         
-        return index if index != inf else -1
-                
+        return -1
