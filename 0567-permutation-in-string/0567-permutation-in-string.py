994f57db197007a5427360bd7a258cfb782a1a05
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-10-10 09:31:42 +0300

    Time: 67 ms (69.78%), Space: 16.3 MB (71.14%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,28 +1,32 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        counter_s1 = {}
+        
+        for char in s1:
+            if char not in counter_s1:
+                counter_s1[char] = 0
+            counter_s1[char] += 1
+        
+        counter_s2 = {}
+        
         start = 0
-        d = {}
-        match = 0
-        for i in s1:
-            if i not in d:
-                d[i] = 0
-            d[i] += 1
         
         for end in range(len(s2)):
-            last = s2[end]
-            if last in d:
-                d[last] -= 1
-                if d[last] == 0:
-                    match += 1
-            if match == len(d):
-                return True
-            if end >= len(s1) - 1:
-                first = s2[start]
-                if first in d:
-                    if d[first] == 0:
-                        match -= 1
-                    d[first] += 1
-                start += 1
+            char = s2[end]
+            
+            if char not in counter_s2:
+                counter_s2[char] = 0
+            counter_s2[char] += 1
+            
+            if end >= len(s1):
+                counter_s2[s2[start]] -= 1
+                if counter_s2[s2[start]] == 0:
+                    del counter_s2[s2[start]]
                 
+                start += 1
+            
+            if counter_s1 == counter_s2:
+                return True
+        
+        
         return False
-    
