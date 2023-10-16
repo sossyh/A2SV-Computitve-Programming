3baaa4ba320bfd9f60385067ff702438c080c6d7
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-10-16 03:11:06 +0300

    Time: 33 ms (83.49%), Space: 16.3 MB (54.17%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,34 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        final_string = ""
+        stack = []
+        curr = ""
+        curr_digit = 0
+      
+        
+        for i in s:
+            if i.isnumeric():
+                curr_digit = 10 * curr_digit + int(i)
+            
+            elif i == '[':
+                stack.append(curr_digit)
+                stack.append("[")
+                curr_digit = 0
+            
+            elif i == "]":
+                curr = ""
+                while stack and stack[-1] != '[':
+                    curr = stack.pop() + curr
+                stack.pop()
+                freq = stack.pop()
+                new_substring = freq * curr
+                stack.append(new_substring)
+            
+            else:
+                stack.append(i)
+        
+        
+        return "".join(stack)
+                
+            
