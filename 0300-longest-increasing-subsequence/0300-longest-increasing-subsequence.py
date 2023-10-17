ef8f7316ffcb54b84092d6e2ec56803ac7ccd72a
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-10-17 11:42:58 +0300

    Time: 4097 ms (5.00%), Space: 20.8 MB (5.15%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,24 +1,25 @@
 class Solution:
-    def dp(self, idx, nums, prev, memo):
-        if idx in memo:
-            return memo[idx]
-        
-        if idx >= len(nums):
-            return 0
-        
-        longest = 0
-        
-        for i in range(idx, len(nums)):
-            if nums[i] > prev:
-                curr = self.dp(i+1, nums, nums[i], memo)
-                longest = max(longest, curr + 1)
-        
-        memo[idx] = longest
-        return longest
-        
-        
     def lengthOfLIS(self, nums: List[int]) -> int:
-        memo = {}
-        return self.dp(0, nums, -inf, memo)
         
+        memo = {}
+        
+        def dp(idx, lst):
+            if idx in memo:
+                return memo[idx]
+            
+            if idx >= len(nums):
+                return 0
+            
+            max_length = 0
+            for i in range(idx, len(nums)):
+                if not lst or nums[i] > lst[-1]:
+                    lst.append(nums[i])
+                    a = dp(i + 1, lst)
+                    max_length = max(max_length, a + 1)
+                    lst.pop()
+            
+            memo[idx] = max_length
+            return max_length
+        
+        return dp(0, [])
         
