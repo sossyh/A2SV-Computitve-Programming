28d7e38903d47741352996610ee567be69e3aad3
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-10-17 10:25:00 +0300

    Time: 63 ms (75.15%), Space: 16.2 MB (96.35%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -5,9 +5,7 @@
 #         self.next = next
 class Solution:
     def reverse(self, head):
-        
-        prev = None
-        curr = head
+        prev, curr = None, head
         
         while curr:
             n = curr.next
@@ -16,49 +14,48 @@ class Solution:
             curr = n
         
         return prev
-            
+    
+    def make_linkedlist(self, stack):
+        head = ListNode()
+        dummy =  head
         
-    def makeListNode(self, arr):
-        head = ListNode(0)
-        dummy = head
-        
-        while arr:
-            a = arr.pop()
-            node = ListNode(a)
-            dummy.next = (node)
+        while stack:
+            val = stack.pop()
+            dummy.next = ListNode()
+            dummy.next.val = val
             dummy = dummy.next
         
         return head.next
-            
-        
+    
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
+        l1 = self.reverse(l1)
+        l2 = self.reverse(l2)
         stack = []
-        newl1 = self.reverse(l1)
-        newl2 = self.reverse(l2)
+        
         carry = 0
         
-        while newl1 and newl2:
-            a = newl1.val + newl2.val + carry
-            stack.append(a%10)
-            carry = a // 10
-            newl1 = newl1.next
-            newl2 = newl2.next
         
-        while newl1:
-            a = carry + newl1.val
-            stack.append(a%10)
-            carry = a // 10
-            newl1 = newl1.next
+        while l1 and l2:
+            total = l1.val + l2.val + carry
+            stack.append(total % 10)
+            carry = total // 10
+            l1 = l1.next
+            l2 = l2.next
         
-        while newl2:
-            a = carry + newl2.val
-            stack.append(a%10)
-            carry = a // 10
-            newl2 = newl2.next
+        while l1:
+            total = l1.val + carry
+            stack.append(total % 10)
+            carry = total // 10
+            l1 = l1.next
+        
+        while l2:
+            total = l2.val + carry
+            stack.append(total % 10)
+            carry = total // 10
+            l2 = l2.next
         
         if carry:
             stack.append(carry)
         
-        
-        return self.makeListNode(stack)
+        return (self.make_linkedlist(stack))
+       
