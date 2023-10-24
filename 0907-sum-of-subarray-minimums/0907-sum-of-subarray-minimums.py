abca2606c6001ef3f3d46e3fa9e3b99a62db0fc4
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-10-24 20:20:32 +0300

    Time: 402 ms (46.03%), Space: 20.9 MB (64.10%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,27 +1,19 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
         arr.append(0)
-        summ = 0
         stack = []
+        total = 0
         
         for i in range(len(arr)):
-            currlb = i - 1
-            
+            curr_lb = i - 1
             while stack and arr[stack[-1][1]] > arr[i]:
-                element = stack.pop()
-                
-                leftspan = element[1] - element[0]
-                rightspan = i - element[1]
-                
-                freq = leftspan * rightspan
-                
-                summ += freq * arr[element[1]]
-                
-                currlb = element[0]
-                
+                prev, curr = stack.pop()
+                left = curr - prev
+                right = i - curr
+                freq = (right * left)
+                total += freq * arr[curr]
+                curr_lb = prev
             
-            
-            stack.append((currlb, i))
+            stack.append((curr_lb, i))
         
-        return summ % (pow(10,9)+7)
-                
+        return (total) % (pow(10, 9) + 7)
