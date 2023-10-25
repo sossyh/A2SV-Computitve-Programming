5e4de3bfc93a4957db8ab4fd5c355f0abd61aa33
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-10-25 03:32:09 +0300

    Time: 33 ms (85.18%), Space: 16 MB (97.42%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -3,17 +3,16 @@ class Solution:
         if n == 1:
             return 0
         
-        a = self.kthGrammar(n-1, math.ceil(k/2))
+        parent = self.kthGrammar(n - 1, math.ceil(k / 2))
         
-        if a == 0 and k % 2 == 1:
+        if parent == 0 and k % 2 == 1:
             return 0
         
-        elif a == 0 and k % 2 == 0:
+        elif parent == 0 and k % 2 == 0:
             return 1
         
-        elif a == 1 and k % 2 == 1:
+        elif parent == 1 and k % 2 == 1:
             return 1
         
         else:
             return 0
-            
