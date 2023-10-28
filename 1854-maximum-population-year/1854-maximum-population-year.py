72fa47dd3e4f30b045e81efc6b9b36695fa41aeb
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-10-28 12:12:16 +0300

    Time: 53 ms (34.00%), Space: 16.3 MB (30.95%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.py
+++ 1854-maximum-population-year/1854-maximum-population-year.py
@@ -1,22 +1,27 @@
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
-        prefix = [0] * 2051
+        max_yr = max([i for k, i in logs])
+        min_yr = min([i for i, j in logs])
+        length_prefix = max_yr - min_yr + 1
+        prefix = [0] * length_prefix
+        offset = min_yr
         
         for i, j in logs:
-            prefix[i] += 1
-            prefix[j] -= 1
+            
+            prefix[i - offset] += 1
+            prefix[j - offset] -= 1
         
         prefixsum = [prefix[0]]
-        
-        for i in range(1, 2051):
+   
+        for i in range(1, len(prefix)):
             prefixsum.append(prefixsum[i - 1] + prefix[i])
         
         max_people_start_year = 0
         max_people = 0
         
-        for i in range(2051):
+        for i in range(len(prefixsum)):
             if prefixsum[i] > max_people:
                 max_people_start_year = i
                 max_people = prefixsum[i]
         
-        return max_people_start_year
+        return max_people_start_year + offset
