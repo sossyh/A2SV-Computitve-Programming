bb6f90897d37a94665d04f85cd405a2523e0ca44
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2023-11-03 08:32:44 +0300

    Time: 211 ms (39.29%), Space: 17.6 MB (79.00%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.py
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -2,28 +2,20 @@ class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
         freq = Counter(nums)
         degree = 0
+        d = {}
         
-        for i in freq:
-            degree = max(degree, freq[i])
+        for i in range(len(nums)):
+            degree = max(freq[nums[i]], degree)
+            if nums[i] not in d:
+                d[nums[i]] = [i, i]
+            else:
+                d[nums[i]][1] = i
         
         min_len = inf
-        start = 0
-        d = Counter()
-        curr_degree = 0
         
-        for end in range(len(nums)):
-            elet = nums[end]
-            d[elet] += 1
-            curr_degree = max(curr_degree, d[elet])
-            
-            while curr_degree >= degree:
-                if curr_degree == degree:
-                    min_len = min(min_len, end - start + 1)
-                
-                left = nums[start]
-                if d[left] == curr_degree:
-                    curr_degree -= 1
-                d[left] -= 1
-                start += 1
+        for i in d:
+            if freq[i] == degree:
+                min_len = min(min_len, d[i][1] - d[i][0] + 1)
         
         return min_len
+            
