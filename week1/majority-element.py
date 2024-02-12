9c204446ae47d774b8ce0bb59e3993aaa651e47f
Sosna W.Achamyeleh <88205224+sossyh@users.noreply.github.com>
2024-02-12 11:12:10 +0300

    Added solution for Majority Element

---
+++ week1/majority-element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        element = None
+
+        for num in nums:
+            if count == 0:
+                element = num
+                count = 1
+            elif element == num:
+                count += 1
+            else:
+                count -= 1
+        
+        return element
+
